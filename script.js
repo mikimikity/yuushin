43bbd07b17268aefae753a834f2b76e5e868e1dd
mikity <miki.mikimiki06@gmail.com>
2017-02-09 19:31:30 +0900

    headerの黒いバーがいい位置で出現するようになったよ

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-	var $win = $(window),
-	    $header = $('header'),
-	    animationClass = 'is-animation';
+    var $win = $(window),
+        $header = $('header'),
+        animationClass = 'is-animation';
 
-	$win.on('load scroll', function() {
-		var value = $(this).scrollTop();
-		if ( value > 100 ) {
-			$header.addClass(animationClass);
-		} else {
-			$header.removeClass(animationClass);
-		}
-	});
+    $win.on('load scroll', function() {
+        var value = $(this).scrollTop();
+    var off = $('section.news').offset();
+        if ( value > off.top-100 ) {
+            $header.addClass(animationClass);
+        } else {
+            $header.removeClass(animationClass);
+        }
+    });
 });
